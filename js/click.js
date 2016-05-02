989130d4ab1d95b46586e57d62da0453d3a490d8
TIMMY HALL <TIMMY_HALL@ICLOUD.COM>
2016-05-02 11:39:38 -0400

    Modified index.html and click.js using knockout. Renamed images

--- js/click.js
+++ js/click.js
@@ -1,13 +1,34 @@
-function loadData() { 
-var cat = $('.cat');
-var counter = $('#counter');
+var Cat = function() { 
+	this.name = ko.observable('Kitty');
+	this.clickCount = ko.observable(0);
+	this.imgSrc= ko.observable('img/kitty.jpg');
+	this.levels = ko.computed(function() {
+		var levels;
+		var clicks = this.clickCount();
+		if (clicks < 10) {
+			levels = 'Newborn';
+		} else if (clicks < 20) { 
+			levels = 'Infant';
+		} else {
+			levels = 'Teen';
+		}
+		return levels; 
+	}, this);
+	this.nicknames = ko.observableArray([
+		{ nick: 'K' },
+		{ nick: 'K Wow' },
+		{ nick: 'K Meow' }
+		]);
+}
 
+var ViewModel = function() {
+	this.currentCat = ko.observable( new Cat() );
 
+	this.incrementCounter = function () { 
+		this.clickCount(this.clickCount() + 1);
+	};
 
-cat.click(function() {
-    counter.html(function(i, val) { return val*1+1 });
-});
 
 }
 
-$('.container').submit(loadData);
+ko.applyBindings(new ViewModel());
